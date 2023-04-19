24d2b8fe3a5904a0504b5f7a382ca8df68a86767
marijacetkovic <115401358+marijacetkovic@users.noreply.github.com>
2023-04-19 14:24:30 +0200

    prompt update

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+
+
+
 var button = document.querySelector(".roll");
 button.addEventListener("click", function() {
+
+var name1 = prompt("What's your name player 1?", "Player 1");
+var name2 = prompt("What's your name player 2?", "Player 2");
+document.querySelector(".player1 p").textContent = name1;
+document.querySelector(".player2 p").textContent = name2;
 
 var randomNumber1 = Math.floor(Math.random() * 6 +1);
 var randomDice1 = "diceimgs/dice"+randomNumber1+".png";
@@ -11,11 +19,11 @@ var img2 = document.querySelector(".player2 img");
 img2.setAttribute("src", randomDice2);
 var text = document.querySelector("h1");
 if (randomNumber1>randomNumber2) {
-	text.textContent = "player 1 won!";
+	text.textContent = name1 + " won!";
 	text.style.left = "-4%";
 }
 else if (randomNumber2>randomNumber1){
-	text.textContent = "player 2 won!";
+	text.textContent = name2+ " won!";
 	text.style.left = "-5%";
 
 }
